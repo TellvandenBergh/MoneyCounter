c24010015a04e886f70e704de2597610b4e5d92b
Tell van den Bergh <tellvandenbergh@gmail.com>
2019-05-20 12:46:29 +0200

    Initial Commit

---
+++ MoneyCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyCounter
+//
+//  Created by Tell van den Bergh on 20/05/2019.
+//  Copyright © 2019 Tell van den Bergh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
